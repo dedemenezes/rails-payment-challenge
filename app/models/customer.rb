19746b533eae6c6e98be1a5364adb78bbe215a2c
Andre Menezes <djdedemenezes@gmail.com>
2024-07-16 16:46:45 -0300

    add secure password to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,12 @@
 class Customer < ApplicationRecord
+  has_secure_password
+  has_secure_password :recovery_password, validations: false
+
   validates :email, :first_name, :last_name, :cpf, :balance, presence: true
   validates :cpf, format: { with: /\A\d{11}\z/ }
   validates :cpf, :email, uniqueness: { case_sensitive: false }
   validates :balance, numericality: { only_integer: true }
+  validates :password, length: { minimum: 6 }
 
   before_validation :clean_cpf
   # before_validation :stop
@@ -10,7 +14,7 @@ class Customer < ApplicationRecord
   private
 
   def clean_cpf
-    return if cpf.empty?
+    return unless cpf
 
     self.cpf = cpf.gsub('-', '').gsub('.', '').strip
   end
