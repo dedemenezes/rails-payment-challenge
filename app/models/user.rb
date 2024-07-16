700dfc80b583a3a95591763cafb73d443f32729d
Andre Menezes <djdedemenezes@gmail.com>
2024-07-16 16:02:07 -0300

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :email, presence: true
   has_secure_password
-  has_secure_password :recovery_password, validation: false # allows user to create an account without recovery password
+  has_secure_password :recovery_password, validations: false # allows user to create an account without recovery password
 end
